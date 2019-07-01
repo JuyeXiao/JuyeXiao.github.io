aea2aa1bc803f80568f596cca635f08ceed6e51c
jueyxiao <juyexiao@umich.edu>
2019-07-01 16:45:30 +0800

    try to implement the contact form

--- src/contact.js
+++ src/contact.js
@@ -80,7 +80,7 @@
                     var formData = $($form).serialize();
                     $.ajax({
                         type: 'POST',
-                        url: $($form).attr('action'),
+                        url: '//formspree.io/xjy1998@hotmail.com',
                         data: formData
                     })
                     window.setTimeout(function() {
